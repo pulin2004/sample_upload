7a673cf52d91ca02f020a93d5389131c42a5d909
pulin2004 <pulind@sina.com>
2017-02-14 22:20:32 +0800

    更新枚举

--- upload/order.py
+++ upload/order.py
@@ -35,5 +35,15 @@ class OrderDetail(BaseClass):
         self._attrs = dict()
 
     def dealLine(self, String):
-        pass    
+        pass
 
+
+def enum(**enums):
+    return type('Enum', (), enums)
+
+#订单号,用户名,支付方式(payment),商品总额,配送费用,买家支付积分,订单总额,可得积分,已支付金额,买家实际支付积分,支付状态,订单附言,收货人姓名,收货地址,配送方式,电话,手机,下单日期,付款时间,
+OrderEnum = enum(orderId='_id', consumer='_consumer',payment='_payment',totalAmount='_totalAmount',transportFee='_transportFee',accumulative='_accumulative',
+               orderAmount='_orderAmount', raiseFen='_raiseFen',payed = '_payed',actualFen='_actualFen',payStatus='_payStatus',orderRemark='_orderRemark',receiverName='_receiverName',receiverAddr='_receiverAddr',shipping='_shipping',phone='_phone',mobliePhone='_mobliePhone',orderDate='_orderDate',payDate='_payDate')
+#商品名称,宝贝种类,发货单号,物流公司,订单备注
+OrderDetalEnum = enum(productName='_productName',productType='_productType',shippingNum='_shippingNum',shippinCompany='_shippingCompany',orderRemark='_orderRemark')
+# Numbers.ONE == 1, Numbers.TWO == 2 and Numbers.THREE == 'three'
